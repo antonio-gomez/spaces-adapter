12ead4bdb5a0a03a93bf68aa74169f4f209316f4
Barkin Aygun <volfied@gmail.com>
2015-11-05 15:35:10 -0800

    Add missing lib files to index

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,21 +1,29 @@
 define(function (require, exports) {
 
+exports.application = require("./application");
 exports.artboard = require("./artboard");
+exports.bounds = require("./bounds");
 exports.brushes = require("./brushes");
 exports.color = require("./color");
 exports.contentLayer = require("./contentLayer");
 exports.document = require("./document");
+exports.generator = require("./generator");
 exports.guide = require("./guide");
+exports.history = require("./history");
 exports.hitTest = require("./hitTest");
 exports.layer = require("./layer");
 exports.layerEffect = require("./layerEffect");
+exports.libraries = require("./libraries");
 exports.path = require("./path");
 exports.photoshopEvent = require("./photoshopEvent");
+exports.preference = require("./preference");
 exports.reference = require("./reference");
 exports.ruler = require("./ruler");
+exports.selection = require("./selection");
 exports.shape = require("./shape");
 exports.textLayer = require("./textLayer");
 exports.tool = require("./tool");
 exports.unit = require("./unit");
+exports.vectorMask = require("./vectorMask");
 
 });
