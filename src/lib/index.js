e8313aee014d6643b1d14161c655ba79216d5afe
Barkin Aygun <volfied@gmail.com>
2015-11-06 09:33:30 -0800

    Add index files for exporting things in object form

---
+++ src/lib/index.js
@@ -0,0 +1,28 @@
+define(function (require, exports) {
+    "use strict";
+
+    exports.application = require("./application");
+    exports.artboard = require("./artboard");
+    exports.bounds = require("./bounds");
+    exports.brushes = require("./brushes");
+    exports.color = require("./color");
+    exports.contentLayer = require("./contentLayer");
+    exports.document = require("./document");
+    exports.generator = require("./generator");
+    exports.history = require("./history");
+    exports.hitTest = require("./hitTest");
+    exports.layer = require("./layer");
+    exports.layerEffect = require("./layerEffect");
+    exports.libraries = require("./libraries");
+    exports.path = require("./path");
+    exports.photoshopEvent = require("./photoshopEvent");
+    exports.preference = require("./preference");
+    exports.reference = require("./reference");
+    exports.ruler = require("./ruler");
+    exports.selection = require("./selection");
+    exports.shape = require("./shape");
+    exports.textLayer = require("./textLayer");
+    exports.tool = require("./tool");
+    exports.unit = require("./unit");
+    exports.vectorMask = require("./vectorMask");
+});
