41c0a465433a367b988cb472e7ca7f885992fb86
Seth Walker <sewalker@adobe.com>
2015-11-10 12:30:55 -0600

    radius thing

--- src/lib/vectorMask.js
+++ src/lib/vectorMask.js
@@ -79,7 +79,8 @@ define(function (require, exports) {
     var makeCircularBoundsWorkPath = function (bounds) {
         var cenX = (bounds.left + bounds.right) / 2,
             cenY = (bounds.top + bounds.bottom) / 2,
-            radius = Math.max(bounds.top - cenY, bounds.right - cenX),
+            //radius = Math.min(bounds.top - cenY, bounds.right - cenX),
+            radius = bounds.height > bounds.width ? bounds.width / 2 : bounds.height / 2,
             circleTop = unitLib.pixels(cenY - radius),
             circleLeft = unitLib.pixels(cenX - radius),
             circleRight = unitLib.pixels(cenX + radius),
